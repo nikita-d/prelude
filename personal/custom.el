03b5a9bda0f8185270fe5bbd098dd01ffcb5e9d3
Nikita Dubrovin <nikita.dubrovin@gmail.com>
2013-09-02 14:01:40 +0400

    prelude update

--- personal/custom.el
+++ personal/custom.el
@@ -9,11 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cperl-electric-parens-mark nil)
- '(cperl-extra-perl-args "-Ilib -MModern::Perl=2013")
- '(cperl-highlight-variables-indiscriminately t)
- '(cperl-indent-parens-as-block t)
- '(cperl-mode-hook (quote (er/add-cperl-mode-expansions))))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
